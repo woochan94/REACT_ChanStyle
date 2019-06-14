db2fca595271b9f6b5a5b16d59184f9a65eac36f
정우찬 <vdncks25@naver.com>
2019-06-14 19:16:39 +0900

    FEAT : Create me resolver

---
+++ Back-End/src/api/User/me/me.js
@@ -0,0 +1,11 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Query: {
+        me:(_, __, { request, isAuthenticated}) => {
+            isAuthenticated(request); 
+            const { user } = request;
+            return prisma.user({ id: user.id });
+        }
+    }
+}
