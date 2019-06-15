bbd7e7f8d25d74a38ff165efc8c5b2cf48a7db08
정우찬 <vdncks25@naver.com>
2019-06-15 19:01:33 +0900

    FEAT : Create editProduct resolver

--- Back-End/src/api/Product/Product.js
+++ Back-End/src/api/Product/Product.js
@@ -2,6 +2,11 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
     Product: {
-        files: ({ id }) => prisma.product({ id }).files()
+        files: ({ id }) => prisma.product({ id }).files(), 
+        sizes: ({ id }) => prisma.product({ id }).sizes(),
+        colors: ({ id }) => prisma.product({ id }).colors(), 
+        stocks: ({ id }) => prisma.product({ id }).stocks(),
+        productDetailFile: ({ id }) => prisma.product({ id }).productDetailFile(), 
+        productSizeFile: ({ id }) => prisma.product({ id }).productSizeFile()
     }
 }
---
+++ Back-End/src/api/Product/editProduct/editProduct.js
@@ -0,0 +1,106 @@
+import { prisma } from "../../../../generated/prisma-client"; 
+
+export default {
+    // product 기본 정보 수정 
+    Mutation: {
+        editProduct: async (_, args, { request, isAuthenticated }) => {
+            isAuthenticated(request); 
+            const {
+                id, 
+                name, 
+                price,
+                mainCategory,
+                subCategory
+            } = args; 
+            const product = await prisma.$exists.product({ id }); 
+            if (product) {
+                return prisma.updateProduct({
+                    data: {
+                        name, 
+                        price,
+                        mainCategory, 
+                        subCategory
+                    }, 
+                    where: {
+                        id 
+                    }
+                })
+            } else {
+                throw Error("You can't do that");
+            }
+        }, 
+        // 파일 수정 
+        editFile: (_, args) => {
+            console.log(args);
+            const { id, url } = args; 
+            return prisma.updateFile({
+                data: {
+                    url
+                }, 
+                where: {
+                    id 
+                }
+            })
+        }, 
+        // 사이즈 수정 
+        editSize: (_, args) => {
+            const { id, size } = args;
+            return prisma.updateSize({
+                data: {
+                    size
+                },
+                where: {
+                    id
+                }
+            })
+        },
+        // 색상 수정
+        editColor: (_, args) => {
+            const { id, color } = args;
+            return prisma.updateColor({
+                data: {
+                    color
+                },
+                where: {
+                    id
+                }
+            })
+        },
+        // 재고 수정 
+        editStock: (_, args) => {
+            const { id, stock } = args;
+            return prisma.updateStock({
+                data: {
+                    stock
+                },
+                where: {
+                    id
+                }
+            })
+        }, 
+        // 상품디테일 파일 수정 
+        editProductDetailFile: (_, args) => {
+            const { id, productDetailFile } = args;
+            return prisma.updateProductDetailFile({
+                data: {
+                    productDetailFile
+                },
+                where: {
+                    id
+                }
+            })
+        }, 
+        // 사이즈 파일 수정 
+        editProductSizeFile: async (_, args) => {
+            const { id, productSizeFile } = args;
+            return prisma.updateProductSizeFile({
+                data: {
+                    productSizeFile
+                },
+                where: {
+                    id
+                }
+            })
+        }
+    }
+}
