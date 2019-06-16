c028f4d6b3b9af6e92b65bbd344646924b014753
정우찬 <vdncks25@naver.com>
2019-06-16 23:27:10 +0900

    FEAT : Apply globalStyles and set Theme
    globalstyle 적용 및 theme 세팅 
    - styled-reset 설치

---
+++ Front-End/src/Components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from '../Styles/Theme';
+
+export default () => (
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+  </ThemeProvider>
+)
