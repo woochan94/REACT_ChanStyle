ccde95acddbf0cb434e70127de6ded08f319c629
정우찬 <vdncks25@naver.com>
2019-06-17 21:02:56 +0900

    STYLE : Footer UI

---
+++ Front-End/src/Components/Footer.js
@@ -0,0 +1,45 @@
+import React from "react"; 
+import styled from "styled-components"; 
+
+const Footer = styled.footer`
+    display: flex; 
+    background-color: black;
+    padding: 30px 0 10px;
+`; 
+
+const FooterWrapper = styled.div`
+    width: 100%; 
+    max-width: ${props => props.theme.maxWidth}; 
+    margin: 0 auto;
+    display: flex; 
+    justify-content: center; 
+    align-items: center; 
+    flex-direction: column;
+`;
+
+const GIT = styled.div`
+    margin-bottom: 15px;
+    a{
+        color: white;
+    }
+    svg {
+        color: white;
+    }
+`;
+
+
+
+const Copyright = styled.div`
+    color: white;
+`;
+
+export default () => (
+    <Footer>
+        <FooterWrapper>
+            <GIT>
+                <a href="https://github.com/JeongWooChan/REACT_ChanStyle">GitHub 바로가기 </a>
+            </GIT>
+            <Copyright>Copyright &copy; {new Date().getFullYear()}. All Rights Reserved.</Copyright>
+        </FooterWrapper>
+    </Footer>
+)
