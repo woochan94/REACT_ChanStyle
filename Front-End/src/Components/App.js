3f71b8efbd6f09953c7c356281c7ac3c9e5a67fb
정우찬 <vdncks25@naver.com>
2019-06-17 14:40:06 +0900

    FEAT : Apply react router
    - routes 경로 설정 
    - HashRouter를 이용하여 Router 설정 
    - prop-types 설치

--- Front-End/src/Components/App.js
+++ Front-End/src/Components/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from '../Styles/Theme';
+import AppRouter from './Routes';
+import { HashRouter as Router } from "react-router-dom";
 
 export default () => (
   <ThemeProvider theme={Theme}>
-    <GlobalStyles />
+    <>
+      <GlobalStyles />
+      <Router>
+        <AppRouter isLoggedIn={true} isAdmin={false}/>
+      </Router>
+    </>
   </ThemeProvider>
 )
---
+++ Front-End/src/Components/Routes.js
@@ -0,0 +1,27 @@
+import React from "react"; 
+import PropTypes from "prop-types"; 
+import { Route, Switch } from "react-router-dom"; 
+import Main from "../Routes/Main/Main";
+import MyPage from "../Routes/Mypage/MyPage";
+import Auth from "../Routes/Auth/Auth";
+import Store from "../Routes/Store/Store";
+import Product from "../Routes/Product/Product";
+import Payment from "../Routes/Payment/Payment";
+import Admin from "../Routes/Admin/Admin";
+
+const AppRouter = ({ isLoggedIn, isAdmin }) => (
+    <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/store" component={Store} />
+        <Route exact path="/payment" component={Payment} />
+        <Route path = "/product/:productname" component={Product} />
+        <Route path = "/:username" component={ isLoggedIn ? (isAdmin ? Admin : MyPage) : Auth} />
+    </Switch>
+); 
+
+// Router는 항상 proptypes를 가져야 함 
+AppRouter.propTypes = {
+    isLoggedIn: PropTypes.bool.isRequired
+}
+
+export default AppRouter;
