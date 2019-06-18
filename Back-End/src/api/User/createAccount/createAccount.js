12e765700deab21090517e18386e7ef1e447d44b
정우찬 <vdncks25@naver.com>
2019-06-18 18:53:14 +0900

    FEAT : Apply password encryption using bcrypt
    - pbkdf2 제거 
    - bcrypt를 이용하여 암호화 새로 함

--- Back-End/src/api/User/createAccount/createAccount.js
+++ Back-End/src/api/User/createAccount/createAccount.js
@@ -1,33 +1,29 @@
-import { prisma } from "../../../../generated/prisma-client"; 
-import pbkdf2 from "pbkdf2-password"; 
-
-const hasher = pbkdf2(); 
+import { prisma } from "../../../../generated/prisma-client";
+import bcrypt from "bcrypt";
 
 export default {
     Mutation: {
         createAccount: async (_, args) => {
-            const { name, email, password, zipCode, address, addressDetail, phone } = args; 
-            // 이메일 중복확인을 위한 코드 
+            const { name, email, password, zipCode, address, addressDetail, phone } = args;
+            // 이메일 중복확인을 위한 코드
             // 입력한 이메일이 이미 db에 있으면 true값을 반환
-            const exists = await prisma.$exists.user({
-                email
-            }); 
+            const exists = await prisma.$exists.user({ email });
             if (exists) {
-                throw Error("This email is already taken"); 
+                throw Error("This email is already taken");
             } else {
-                // // 비밀번호 암호화 
-                // hasher({ password }, async (_, __, salt, hasher) => {
-                    
-                // })
-                await prisma.createUser({
-                    name, 
-                    email,
-                    password,
-                    zipCode,
-                    address,
-                    addressDetail,
-                    phone
+                // 비밀번호 암호화 진행
+                bcrypt.hash(password, 10, async function(err, hash) {
+                    await prisma.createUser({
+                        name,
+                        email,
+                        password: hash,
+                        zipCode,
+                        address,
+                        addressDetail,
+                        phone
+                    });
                 });
+
                 return true;
             }
         }
--- Back-End/src/api/User/login/login.js
+++ Back-End/src/api/User/login/login.js
@@ -1,28 +1,21 @@
 import { prisma } from "../../../../generated/prisma-client";
 import { generateToken } from '../../../utils';
-import pbkdf2 from "pbkdf2-password";
-
-const hasher = pbkdf2();
+import bcrypt from 'bcrypt';
 
 export default {
     Mutation: {
-        login: async (_, args, { request }) => {
-            console.log(request.user);
+        login: async (_, args) => {
             const { email, password } = args;
             const user = await prisma.user({ email });
-            // hasher({ password, salt: user.salt }, async (err, pass, salt, hash) => {
-            //     if(hash === user.password) {
-            //         // 여기서 토큰이 발행되어야 하는데 안됨 ... 
-            //         return await generateToken(user.id);
-            //     } else {
-
-            //     }
-            // }); 
-            if( user !== null && user.password === password) {
-                return generateToken(user.id); 
-            } else {
-                throw Error("Wrong email secret combination"); 
-            }
+        
+            if( user !== null ) {
+                // 암호확인 
+                if(bcrypt.compareSync(password, user.password)) {
+                    return generateToken(user.id); 
+                } else {
+                    throw Error("Wrong email/Password combination");
+                }
+            } 
         }
     }
 }
