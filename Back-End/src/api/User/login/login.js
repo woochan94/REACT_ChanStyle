dfc2e14a91b9cae7768ac84da16278f64238a437
정우찬 <vdncks25@naver.com>
2019-06-18 19:29:44 +0900

    FEAT : Apply password regular expression in Login
    - 로그인 시 비밀번호 정규식 적용

--- Back-End/src/api/User/login/login.js
+++ Back-End/src/api/User/login/login.js
@@ -2,20 +2,28 @@ import { prisma } from "../../../../generated/prisma-client";
 import { generateToken } from '../../../utils';
 import bcrypt from 'bcrypt';
 
+// 비밀번호 정규식 설정 (영문, 숫자, 특수문자 조합, 8~16자리)
+const chk_password = /^(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9])(?=.*[0-9]).{8,16}$/;
+
 export default {
     Mutation: {
         login: async (_, args) => {
             const { email, password } = args;
             const user = await prisma.user({ email });
-        
             if( user !== null ) {
-                // 암호확인 
+                // 비밀번호 정규식 확인 
+                if (chk_password.test(password) === false) {
+                    throw Error("비밀번호는 영문, 숫자, 특수문자 조합의 8~16자리여야 합니다."); 
+                }
+                // 비밀번호 암호화 확인  
                 if(bcrypt.compareSync(password, user.password)) {
                     return generateToken(user.id); 
                 } else {
                     throw Error("Wrong email/Password combination");
                 }
-            } 
+            } else {
+                throw Error("존재하지 않는 이메일입니다.");
+            }
         }
     }
 }
