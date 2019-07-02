8137dcf5c57bdfc84d313446c4a9ccf909e83ade
정우찬 <vdncks25@naver.com>
2019-07-02 22:47:57 +0900

    FIX : Change cart into an array data model
    - Cart를 여러개의 입력값을 처리할 수 있도록 배열로 바꿈

--- Back-End/src/api/Cart/addCart/addCart.js
+++ Back-End/src/api/Cart/addCart/addCart.js
@@ -2,28 +2,46 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
     Mutation: {
-        addCart: async (_, args, { request, isAuthenticated}) => {
-            isAuthenticated(request); 
-            const { user } = request; 
-            const { product, size, sizeId, color, colorId, count, countId } = args; 
-            return prisma.createCart({
-                user: {
-                    connect: {
-                        id: user.id
-                    }
-                }, 
-                product: {
-                    connect: {
-                        id: product
-                    }
-                }, 
-                size, 
-                color, 
-                count,
-                sizeId,
-                colorId,
-                countId
-            }) 
+        addCart: async (_, args, { request, isAuthenticated }) => {
+            isAuthenticated(request);
+            const { user } = request;
+            const { product, sizeId, colorId, stockId } = args;
+            try {
+                await product.map(async (_, index) => {
+
+                    await prisma.createCart({
+                        user: {
+                            connect: {
+                                id: user.id
+                            }
+                        },
+                        product: {
+                            connect: {
+                                id: product[index]
+                            }
+                        },
+                        sizeId: {
+                            connect: {
+                                id: sizeId[index]
+                            }
+                        },
+                        colorId: {
+                            connect: {
+                                id: colorId[index]
+                            }
+                        },
+                        stockId: {
+                            connect: {
+                                id: stockId[index]
+                            }
+                        },
+
+                    })
+                })
+                return true;
+            } catch (error) {
+                return false
+            }
         }
     }
 }
