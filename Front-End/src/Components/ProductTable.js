3127196c03b587cbe68f631aa4d53239a3708758
정우찬 <vdncks25@naver.com>
2019-07-04 22:25:15 +0900

    FEAT : Create productList component
    - 장바구니의 상품 테이블을 컴포넌트로 변경

---
+++ Front-End/src/Components/ProductTable.js
@@ -0,0 +1,237 @@
+import React from "react"; 
+import styled from "styled-components"; 
+import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
+import Button from "../Components/Button";
+import { SelectedCount, SelectedCountTextDiv, SelectedCountBtnDiv } from "../Routes/Product/ProductPresenter";
+
+const Table = styled.table`
+    width: 100%;
+    img {
+        width: 130px;
+        height: 150px;
+    }
+    td, th {
+        padding: 10px; 
+        vertical-align: middle;
+    }
+    td {
+        text-align: center;
+        vertical-align: middle;
+    }
+    tbody {
+        tr {
+            border-bottom: 1px solid #ccc;
+        }
+        #emptyTd {
+            padding: 50px 0;
+            @media (max-width: 600px) {
+                display: block;
+                border-radius: 10px; 
+                background-color: transparent; 
+                border-color: #ccc;
+            }
+        }
+    }
+    tfoot {
+        tr {
+            border-bottom: 1px solid #ccc;
+        }
+        @media (max-width: 600px) {
+            display: none;
+        }
+    }
+    @media (max-width: 600px) {
+        display: block;
+        thead {
+            display: none;
+        }
+        img {
+            width: 100px;
+        }
+        tr {
+            display: block;
+            margin: 0.5rem 0;
+            padding: 0;
+            width: 100%;
+            position:relative;
+            border-radius: 10px;
+            border: 1px solid #ccc;
+        }
+        tbody {
+            display: flex; 
+            flex-direction: column; 
+            justify-content: center;
+            align-items: center;
+        }
+        td:first-child {
+            position: relative;
+            top: 0;
+            transform: translateY(0);
+            width: 100%;
+            background-color: ${props => props.theme.confirmColor};
+            border-radius: 10px 10px 0 0;
+            border-color: ${props => props.theme.confirmColor}; 
+            border : 1px solid;
+        }
+        td {
+            display: block;
+            :nth-child(2) {
+                position:absolute !important;
+                left: 0;
+                width: auto !important;
+                height: 180px;
+;
+                border-right: 1px solid #ccc;
+            }
+        }
+        td {
+            :not(:first-child):not(:nth-child(2)) {
+                position: relative;
+                margin-left: 125px;
+                padding: 5px 1em; 
+                text-align: left; 
+                width: 100%;
+            }
+        }
+        td:not(:first-child):before {
+            content: '';
+            display: block;
+            left: 0;
+            position: relative;
+            font-size: .8em;
+            padding-bottom: 0.3em;
+            text-align: left;
+            color: darkgray;
+        }
+        td:nth-child(3):before {
+            content: 'Name (Option)';
+        }
+        td:nth-child(4):before {
+            content: 'Price';
+        }
+        td:nth-child(5):before {
+            content: 'Quantity';
+        }
+    }   
+`;
+
+const Thead = styled.thead`
+    background-color: ${props => props.theme.confirmColor}; 
+    color: white; 
+`;
+
+const Th = styled.th`
+    padding: 10px; 
+    vertical-align: middle;
+`;
+
+const SelectedCount2 = styled(SelectedCount)`
+    @media (max-width: 600px) {
+        margin-left: 0;
+        justify-content: left;
+    }
+`;
+
+const SelectedCountTextDiv2 = styled(SelectedCountTextDiv)`
+    padding: 5px 15px;
+    @media (max-width: 600px) {
+        width: auto;
+    }
+`;
+
+const ResponsiveTotalDiv = styled.div``;
+
+const ProductTable =({
+    allCheck,
+    cartData,
+    count,
+    cartCountUp,
+    cartCountDown,
+    passCartId,
+    total,
+    selectOrder
+}) => {
+    return (
+        <>
+            <Table>
+                <Thead>
+                    <tr>
+                        {allCheck && <Th scope="col"><input type="checkBox" onChange={(e) => allCheck(e.target.checked)}/></Th>}
+                        <Th scope="col">product</Th>
+                        <Th scope="col">Name (Option) </Th>
+                        <Th scope="col">price</Th>
+                        <Th scope="col">Quantity</Th>
+                        {allCheck && <Th scope="col">select</Th>}
+                    </tr>
+                </Thead>
+                <tbody>
+                    {cartData && cartData.seeCart.length === 0 && (
+                        <tr>
+                            <td id={"emptyTd"} colSpan="6">장바구니가 비어있습니다.</td>
+                        </tr>
+                    )}
+                    {cartData && cartData.seeCart.map((item, index) => (
+                        item.product.map((product, index2) => (
+                            product.files.map(file => (
+                                <tr key={index}>
+                                    <td><input id={item.id} type="checkBox" onChange={(e) => e.target.checked} /></td>
+                                    <td>
+                                        <Link to={`/product/${product.id}`}>
+                                            <img src={file.url} alt={file.id} />
+                                        </Link>
+                                    </td>
+                                    <td>
+                                        <Link to={`/product/${product.id}`}>
+                                            <div>
+                                                {product.name}
+                                            </div>
+                                            {item.sizeId[index2].size}/{item.colorId[index2].color}
+                                        </Link>
+                                    </td>
+                                    <td>{product.price}</td>
+                                    <td>
+                                        <SelectedCount2>
+                                            <SelectedCountTextDiv2>{count[index]}</SelectedCountTextDiv2>
+                                            <SelectedCountBtnDiv>
+                                                <Button text={"▲"} onClick={() => cartCountUp(index)}/>
+                                                <Button text={"▼"} onClick={() => cartCountDown(index)} />
+                                            </SelectedCountBtnDiv>
+                                        </SelectedCount2>
+                                    </td>
+                                    <td><button onClick={() => passCartId(item.id)}>삭제</button></td>
+                                </tr>
+                            ))
+                        ))
+                    ))}
+                </tbody>
+                {!isNaN(total) && (
+                    <tfoot>
+                        <tr>
+                            <td colSpan="1">Total</td>
+                            <td colSpan="4"></td>
+                            <td colSpan="1">{total}</td>
+                        </tr>
+                    </tfoot>
+                )}
+            </Table>
+            <ResponsiveTotalDiv id={"responsiveTotalDiv"}>
+                Total : {total}
+            </ResponsiveTotalDiv>
+            <Button id={"cartOrderBtn"} text={"Order Now"} onClick={() => selectOrder()} />
+        </>
+    )
+}
+
+ProductTable.propTypes = {
+    allCheck: PropTypes.func,
+    cartData: PropTypes.object,
+    count: PropTypes.array, 
+    cartCountUp: PropTypes.func, 
+    cartCountDown: PropTypes.func,
+    passCartId: PropTypes.func,
+    total: PropTypes.number,
+    selectOrder: PropTypes.func
+}
+
+export default ProductTable;
--- Front-End/src/Routes/Mypage/MyPagePresenter.js
+++ Front-End/src/Routes/Mypage/MyPagePresenter.js
@@ -1,12 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import { TitleDiv, H2, SelectedCountBtnDiv, SelectedCountTextDiv, SelectedCount } from "../Product/ProductPresenter";
+import { TitleDiv, H2 } from "../Product/ProductPresenter";
 import Button from "../../Components/Button";
 import { Form } from "../Auth/AuthPresenter";
 import SignUpForm from "../../Components/SignUpForm";
 import Loader from "../../Components/Loader";
-import { Link } from 'react-router-dom';
 import { Pagination } from "semantic-ui-react";
+import ProductTable from "../../Components/ProductTable";
 
 const MyPage = styled.section`
     min-height: 79vh;
@@ -77,133 +77,6 @@ const Article = styled.article`
     margin-top: 60px;
     form {
         margin: 0 auto;
-    }
-`;
-
-const Table = styled.table`
-    width: 100%;
-    img {
-        width: 130px;
-        height: 150px;
-    }
-    td, th {
-        padding: 10px; 
-        vertical-align: middle;
-    }
-    td {
-        text-align: center;
-        vertical-align: middle;
-    }
-    tbody {
-        tr {
-            border-bottom: 1px solid #ccc;
-        }
-        #emptyTd {
-            padding: 50px 0;
-            @media (max-width: 600px) {
-                display: block;
-                border-radius: 10px; 
-                background-color: transparent; 
-                border-color: #ccc;
-            }
-        }
-    }
-    tfoot {
-        tr {
-            border-bottom: 1px solid #ccc;
-        }
-        @media (max-width: 600px) {
-            display: none;
-        }
-    }
-    @media (max-width: 600px) {
-        display: block;
-        thead {
-            display: none;
-        }
-        img {
-            width: 100px;
-        }
-        tr {
-            display: block;
-            margin: 0.5rem 0;
-            padding: 0;
-            width: 100%;
-            position:relative;
-            border-radius: 10px;
-            border: 1px solid #ccc;
-        }
-        tbody {
-            display: flex; 
-            flex-direction: column; 
-            justify-content: center;
-            align-items: center;
-        }
-        td:first-child {
-            position: relative;
-            top: 0;
-            transform: translateY(0);
-            width: 100%;
-            background-color: ${props => props.theme.confirmColor};
-            border-radius: 10px 10px 0 0;
-            border-color: ${props => props.theme.confirmColor}; 
-            border : 1px solid;
-        }
-        td {
-            display: block;
-            :nth-child(2) {
-                position:absolute !important;
-                left: 0;
-                width: auto !important;
-                height: 180px;
-                border-right: 1px solid #ccc;
-            }
-        }
-        td {
-            :not(:first-child):not(:nth-child(2)) {
-                position: relative;
-                margin-left: 125px;
-                padding: 5px 1em; 
-                text-align: left; 
-                width: 100%;
-            }
-        }
-        td:not(:first-child):before {
-            content: '';
-            display: block;
-            left: 0;
-            position: relative;
-            font-size: .8em;
-            padding-bottom: 0.3em;
-            text-align: left;
-            color: darkgray;
-        }
-        td:nth-child(3):before {
-            content: 'Name (Option)';
-        }
-        td:nth-child(4):before {
-            content: 'Price';
-        }
-        td:nth-child(5):before {
-            content: 'Quantity';
-        }
-    }   
-`;
-
-const ResponsiveTotalDiv = styled.div`
-
-`;
-
-const SelectedCountTextDiv2 = styled(SelectedCountTextDiv)`
-    padding: 5px 15px;
-    @media (max-width: 600px) {
-        width: auto;
-    }
-`;
-const SelectedCount2 = styled(SelectedCount)`
-    @media (max-width: 600px) {
-        margin-left: 0;
-        justify-content: left;
     }
 `;
 
@@ -315,73 +188,16 @@ export default ({
                         <Article>
                             {cartLoading === true && <Loader />}
                             {cartLoading === false && (
-                                <>
-                                    <Table>
-                                        <Thead>
-                                            <tr>
-                                                <Th scope="col"><input type="checkBox" onChange={(e) => allCheck(e.target.checked)}/></Th>
-                                                <Th scope="col">product</Th>
-                                                <Th scope="col">Name (Option) </Th>
-                                                <Th scope="col">price</Th>
-                                                <Th scope="col">Quantity</Th>
-                                                <Th scope="col">select</Th>
-                                            </tr>
-                                        </Thead>
-                                        <tbody>
-                                            {cartData.seeCart.length === 0 && (
-                                                <tr>
-                                                    <td id={"emptyTd"} colSpan="6">장바구니가 비어있습니다.</td>
-                                                </tr>
-                                            )}
-                                            {cartData.seeCart.map((item, index) => (
-                                                item.product.map((product, index2) => (
-                                                    product.files.map(file => (
-                                                        <tr key={index} >
-                                                            <td><input id={item.id} type="checkBox" onChange={(e) => e.target.checked} /></td>
-                                                            <td>
-                                                                <Link to={`/product/${product.id}`}>
-                                                                    <img src={file.url} alt={file.id} />
-                                                                </Link>
-                                                            </td>
-                                                            <td>
-                                                                <Link to={`/product/${product.id}`}>
-                                                                    <div>
-                                                                        {product.name}
-                                                                    </div>
-                                                                    {item.sizeId[index2].size}/{item.colorId[index2].color}
-                                                                </Link>
-                                                            </td>
-                                                            <td>{product.price}</td>
-                                                            <td>
-                                                                <SelectedCount2>
-                                                                    <SelectedCountTextDiv2>{count[index]}</SelectedCountTextDiv2>
-                                                                    <SelectedCountBtnDiv>
-                                                                        <Button text={"▲"} onClick={() => cartCountUp(index)}/>
-                                                                        <Button text={"▼"} onClick={() => cartCountDown(index)} />
-                                                                    </SelectedCountBtnDiv>
-                                                                </SelectedCount2>
-                                                            </td>
-                                                            <td><button onClick={() => passCartId(item.id)}>삭제</button></td>
-                                                        </tr>
-                                                    ))
-                                                ))
-                                            ))}
-                                        </tbody>
-                                        {!isNaN(total) && (
-                                            <tfoot>
-                                                <tr>
-                                                    <td colSpan="1">Total</td>
-                                                    <td colSpan="4"></td>
-                                                    <td colSpan="1">{total}</td>
-                                                </tr>
-                                            </tfoot>
-                                        )}
-                                    </Table>
-                                    <ResponsiveTotalDiv id={"responsiveTotalDiv"}>
-                                        Total : {total}
-                                    </ResponsiveTotalDiv>
-                                    <Button id={"cartOrderBtn"} text={"Order Now"} onClick={() => selectOrder()} />
-                                </>
+                                <ProductTable 
+                                    allCheck={allCheck}
+                                    cartData={cartData}
+                                    count={count}
+                                    cartCountUp={cartCountUp} 
+                                    cartCountDown={cartCountDown}
+                                    passCartId={passCartId} 
+                                    total={total}
+                                    selectOrder={selectOrder}
+                                />
                             )}
                         </Article>
                     </>
