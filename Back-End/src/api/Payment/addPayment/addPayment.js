b68dfd6180af97b3555599f3f1750dbe32b3d8e0
정우찬 <vdncks25@naver.com>
2019-07-04 19:07:15 +0900

    FEAT : Create addPayment resolver

---
+++ Back-End/src/api/Payment/addPayment/addPayment.js
@@ -0,0 +1,54 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Mutation: {
+        addPayment : async (_, args, { request }) => {
+            const { user } = request;
+            const { product, size, color, stock, count } = args; 
+            
+            try {
+                await count.map(async(item, index) => {
+                    const countId = await prisma.createCount({
+                        count: item
+                    }); 
+                    await prisma.createPayment({
+                        user: {
+                            connect: {
+                                id: user.id 
+                            }
+                        }, 
+                        product: {
+                            connect: {
+                                id: product[index]
+                            }
+                        }, 
+                        size: {
+                            connect: {
+                                id: size[index]
+                            }
+                        },
+                        color: {
+                            connect: {
+                                id: color[index]
+                            }
+                        }, 
+                        stock: {
+                            connect: {
+                                id: stock[index]
+                            }
+                        }, 
+                        count: {
+                            connect: {
+                                id: countId.id
+                            }
+                        }
+                    })
+                })
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false; 
+            }
+        }
+    }
+}
