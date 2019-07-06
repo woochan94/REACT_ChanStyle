7c9e1dc33e519456d9d33ed5e6f2dc3483ecbb9b
정우찬 <vdncks25@naver.com>
2019-07-06 21:09:01 +0900

    FIX : Fix addBuyList resolver

--- Back-End/src/api/BuyList/BuyList.js
+++ Back-End/src/api/BuyList/BuyList.js
@@ -3,6 +3,9 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
     BuyList: {
         product: ({ id }) => prisma.buyList({ id }).product(),
-        user: ({ id }) => prisma.buyList({ id }).user()
+        user: ({ id }) => prisma.buyList({ id }).user(),
+        size: ({ id }) => prisma.buyList({ id }).size(), 
+        color: ({ id }) => prisma.buyList({ id }).color(), 
+        quantity: ({ id }) => prisma.buyList({ id }).quantity()
     }
 }
--- Back-End/src/api/BuyList/addBuyList/addBuyList.js
+++ Back-End/src/api/BuyList/addBuyList/addBuyList.js
@@ -2,25 +2,62 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
     Mutation: {
-        addBuyList: (_, args, { request, isAuthenticated}) => {
+        addBuyList: async (_, args, { request, isAuthenticated}) => {
             isAuthenticated(request); 
             const { user } = request; 
-            const { product, size, color, count } = args; 
-            return prisma.createBuyList({
-                user: {
-                    connect: {
-                        id: user.id 
-                    }
-                }, 
-                product: {
-                    connect: {
-                        id: product
-                    }
-                }, 
-                size,
-                color,
-                count
-            })
+            const { product, size, color, quantity } = args; 
+            try {
+                await quantity.map(async(item, index) => {
+                    const quantityId = await prisma.createQuantity({
+                        quantity: item
+                    }); 
+                    await prisma.createBuyList({
+                        user: {
+                            connect: {
+                                id: user.id 
+                            }
+                        }, 
+                        product: {
+                            connect: {
+                                id: product[index]
+                            }
+                        }, 
+                        size: {
+                            connect: {
+                                id: size[index]
+                            }
+                        }, 
+                        color: {
+                            connect: {
+                                id: color[index]
+                            }
+                        }, 
+                        quantity: {
+                            connect: {
+                                id: quantityId.id
+                            }
+                        }
+                    })
+                })
+                return true; 
+            } catch {
+                return false; 
+            }
+            // return prisma.createBuyList({
+            //     user: {
+            //         connect: {
+            //             id: user.id 
+            //         }
+            //     }, 
+            //     product: {
+            //         connect: {
+            //             id: product
+            //         }
+            //     }, 
+            //     size,
+            //     color,
+            //     count
+            // })
         }
     }
 }
